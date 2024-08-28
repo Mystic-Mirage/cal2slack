88368aa9b53b0d3e707c20af402af90b89069991
Mystic-Mirage <mm@m10e.net>
2024-08-28 21:13:06 +0300

    Remove test function

--- Calendar.js
+++ Calendar.js
@@ -15,21 +15,3 @@ function listUpcomingEvents() {
 
   return Calendar.Events.list(calendarId, optionalArgs).items;
 }
-
-function testListUpcomingEvents() {
-  const calendarId = "primary";
-
-  const timeMin = new Date("2024-08-26 14:15");
-  const timeMax = new Date(timeMin);
-  timeMax.setSeconds(timeMax.getSeconds() + 1);
-
-  const optionalArgs = {
-    timeMin: timeMin.toISOString(),
-    timeMax: timeMax.toISOString(),
-    showDeleted: false,
-    singleEvents: true,
-    orderBy: "startTime"
-  };
-
-  console.log(Calendar.Events.list(calendarId, optionalArgs).items);
-}
