eff3130099521c119c50bbe1c75bab3822a11d85
Mystic-Mirage <mm@m10e.net>
2024-09-06 13:02:36 +0300

    Sort working hours only if all fields are set

--- OnEdit.js
+++ OnEdit.js
@@ -6,7 +6,11 @@
 function onEdit(e) {
   const sheet = e.range.getSheet();
   if (sheet.getName() === WORKING_HOURS_SHEET) {
-    console.log(sheet.getName());
-    sheet.getRange(1,1,100,3).sort([1, 2, 3]);
+    const row = e.range.getRow();
+    const editRange = sheet.getRange(row, 1, 1, 3);
+    const values = editRange.getValues()[0];
+    if (values.every(value => value) || values.every(value => !value)) {
+      sheet.getRange(1,1,100,3).sort([1, 2, 3]);
+    }
   }
 }
